ef384070bda9a0dd9f63e3cd61adb3ef3443cc8f
v666k0 <k0fanov36@yandex.ru>
2025-12-08 13:36:39 +0300

    public broker funcs

--- server/internal/broker/public.go
+++ server/internal/broker/public.go
@@ -1,1 +1,32 @@
 package broker
+
+import "context"
+
+type Broker struct {
+	Broker IBroker
+}
+
+type IBroker interface {
+	IPublisher
+	IConsumer
+	IExchange
+	IQueue
+}
+
+type IPublisher interface {
+	Publish(_ context.Context, msg Message) error
+}
+
+type IConsumer interface {
+	Consume(_ context.Context) error
+	StopConsuming(_ context.Context) error
+	GetConsumerChannel() chan Message
+}
+
+type IExchange interface {
+	CreateExchange(name string) error
+}
+
+type IQueue interface {
+	CreateQueue(exchange, queue, routingKey string) error
+}
