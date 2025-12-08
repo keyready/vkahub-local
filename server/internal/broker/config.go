65a97457ed2d372ea7d8fdd951dd35483e15e23b
v666k0 <k0fanov36@yandex.ru>
2025-12-08 13:37:45 +0300

    init broker config

--- server/internal/broker/config.go
+++ server/internal/broker/config.go
@@ -1,1 +1,4 @@
 package broker
+
+type Config struct {
+}	
--- server/internal/broker/dto.go
+++ server/internal/broker/dto.go
@@ -1,1 +1,8 @@
 package broker
+
+import "github.com/google/uuid"
+
+type Message struct {
+	EventID uuid.UUID   `json:"eventID"`
+	Body    interface{} `json:"body"`
+}
