057ec304caa7d6904cbf1658d08655171bc4ab78
v666k0 <k0fanov36@yandex.ru>
2025-12-08 13:40:51 +0300

    init broker

--- server/internal/broker/config.go
+++ server/internal/broker/config.go
@@ -1,4 +1,8 @@
 package broker
 
 type Config struct {
-}	
+	Address    string `mapstructure:"address"`
+	Exchange   string `mapstructure:"exchange"`
+	RoutingKey string `mapstructure:"routingKey"`
+	QueueName  string `mapstructure:"queue"`
+}
